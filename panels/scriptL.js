dee39e856c8931bb769c4d5620e6cef6cb03ffb3
Aaron Stannard <aaron@postproduktiv.com.au>
2016-04-26 14:29:11 +1000

    Add panels template

---
+++ panels/scriptL.js
@@ -0,0 +1,134 @@
+(function () {
+
+    // get elements
+
+    var brownArrow = document.getElementById('brownArrowL');
+    var whiteArrow = document.getElementById('whiteArrowL');
+
+    var tt1 = document.getElementById('tt1');
+    var tt2 = document.getElementById('tt2');
+
+    var bale = document.getElementById('bale');
+    var carell = document.getElementById('carell');
+    var gosling = document.getElementById('gosling');
+    var pitt = document.getElementById('pitt');
+
+    var q1 = document.getElementById('q1');
+    var q2 = document.getElementById('q2');
+    var q3 = document.getElementById('q3');
+
+    var baleName = document.getElementById('baleName');
+    var carellName = document.getElementById('carellName');
+    var goslingName = document.getElementById('goslingName');
+    var pittName = document.getElementById('pittName');
+
+    var dvd = document.getElementById('dvd');
+    var bd = document.getElementById('bd');
+    var cta = document.getElementById('cta');
+    var button = document.getElementById('button');
+
+    var copyright = document.getElementById('copyright');
+    var rating = document.getElementById('rating');
+
+    // set vars
+
+    var defaultAnimScale = 0.75;
+    var defaultAnimTime = 1.25;
+    var defaultStaggerOffset = 0.25;
+    var defaultFrameDelay = 3;
+
+    // set defaults
+
+    TweenLite.defaultEase = Power4.easeOut;
+
+    var secs = [0.25, 1, 2, 3, 5];
+
+    var tl1 = new TimelineMax({
+        repeat: 0,
+        repeatDelay: defaultFrameDelay
+    });
+
+    function startAnim() {
+
+        //console.log('startAnim');
+
+        tl1
+
+            .set(brownArrow, {
+            autoAlpha: 1,
+            y: -769
+        })
+
+        .set(whiteArrow, {
+            autoAlpha: 1,
+            y: -362
+        })
+
+        .set(dvd, {
+            autoAlpha: 1,
+            x: -100
+        })
+
+        .set(bd, {
+            autoAlpha: 1,
+            x: 100
+        })
+
+        .staggerTo([brownArrow, whiteArrow], 2, {
+            y: 0
+        }, 0, secs[0])
+
+        .staggerFrom([tt1, tt2], 2, {
+            autoAlpha: 0,
+        }, 1, secs[1])
+
+        .staggerFrom([baleName, carellName, goslingName, pittName], defaultAnimTime, {
+            autoAlpha: 0,
+            y: 25
+        }, defaultStaggerOffset, secs[2])
+
+        .staggerTo([dvd, bd], 0.75, {
+            x: 0
+        }, defaultStaggerOffset, secs[3])
+
+        .staggerFrom([cta, button], 0.75, {
+            autoAlpha: 0,
+            y: 25
+        }, defaultStaggerOffset, secs[3])
+
+        .from(copyright, 1.5, {
+            autoAlpha: 0,
+        }, secs[4])
+
+        .to(button, 0.5, {
+                scale: 1.125,
+                ease: Power4.easeInOut
+            })
+            .to(button, 2, {
+                scale: 1,
+                ease: Power4.easeInOut
+            })
+
+
+        ;
+
+    }
+
+    // init
+
+    function addEventListeners() {
+        //console.log('addEventListeners');
+    }
+
+    function initBanner() {
+        //console.log('initBanner');
+
+        bannerContainer.style.display = 'block';
+
+        addEventListeners();
+        startAnim();
+    }
+
+    initBanner();
+
+})();
---
+++ panels/scriptR.js
@@ -0,0 +1,111 @@
+(function () {
+
+    // get elements
+
+    var brownArrow = document.getElementById('brownArrowR');
+    var whiteArrow = document.getElementById('whiteArrowR');
+
+    var tt1 = document.getElementById('tt1');
+    var tt2 = document.getElementById('tt2');
+
+    var bale = document.getElementById('bale');
+    var carell = document.getElementById('carell');
+    var gosling = document.getElementById('gosling');
+    var pitt = document.getElementById('pitt');
+
+    var q1 = document.getElementById('q1');
+    var q2 = document.getElementById('q2');
+    var q3 = document.getElementById('q3');
+
+    var baleName = document.getElementById('baleName');
+    var carellName = document.getElementById('carellName');
+    var goslingName = document.getElementById('goslingName');
+    var pittName = document.getElementById('pittName');
+
+    var dvd = document.getElementById('dvd');
+    var bd = document.getElementById('bd');
+    var cta = document.getElementById('cta');
+    var button = document.getElementById('button');
+
+    var copyright = document.getElementById('copyright');
+    var rating = document.getElementById('rating');
+
+    // set vars
+
+    var defaultAnimScale = 0.75;
+    var defaultAnimTime = 1.25;
+    var defaultStaggerOffset = 0.25;
+    var defaultFrameDelay = 3;
+
+    // set defaults
+
+    TweenLite.defaultEase = Power4.easeOut;
+
+    var secs = [0.25, 1, 2, 3, 5];
+
+    var tl1 = new TimelineMax({
+        repeat: 0,
+        repeatDelay: defaultFrameDelay
+    });
+
+    function startAnim() {
+
+        //console.log('startAnim');
+
+        tl1
+
+            .set(brownArrow, {
+            autoAlpha: 1,
+            y: -769
+        })
+
+        .set(whiteArrow, {
+            autoAlpha: 1,
+            y: -362
+        })
+
+        .staggerTo([brownArrow, whiteArrow], 2, {
+            y: 0
+        }, 0, secs[0])
+
+        .staggerFrom([tt1, tt2], 2, {
+            autoAlpha: 0,
+        }, 1, secs[1])
+
+        .staggerFrom([bale, carell, gosling, pitt], defaultAnimTime, {
+            autoAlpha: 0,
+            y: 25
+        }, defaultStaggerOffset, secs[2])
+
+        .staggerFrom([q1, q2, q3], 0.75, {
+            autoAlpha: 0,
+            y: 25
+        }, defaultStaggerOffset, secs[3])
+
+
+        .from(rating, 1.5, {
+            autoAlpha: 0,
+        }, secs[4])
+
+        ;
+
+    }
+
+    // init
+
+    function addEventListeners() {
+        //console.log('addEventListeners');
+    }
+
+    function initBanner() {
+        //console.log('initBanner');
+
+        bannerContainer.style.display = 'block';
+
+        addEventListeners();
+        startAnim();
+    }
+
+    initBanner();
+
+})();
