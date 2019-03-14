3e8b7175d5f9a1f60181a36dcadaebaccc029a82
Aaron Stannard <aaron@postproduktiv.com.au>
2019-03-14 16:37:29 +1100

    Clean up

--- standard/in-page-with-text/script.js
+++ standard/in-page-with-text/script.js
@@ -14,20 +14,20 @@
     });
     var quote1SplitChars = quote1Split.chars;
     var quote1SplitWords = quote1Split.words;
-    
-        var quote2Split = new SplitText(quote2, {
+
+    var quote2Split = new SplitText(quote2, {
         type: "chars,words"
     });
     var quote2SplitChars = quote2Split.chars;
     var quote2SplitWords = quote2Split.words;
-    
-        var ctaSplit = new SplitText(cta, {
+
+    var ctaSplit = new SplitText(cta, {
         type: "chars,words"
     });
     var ctaSplitChars = ctaSplit.chars;
     var ctaSplitWords = ctaSplit.words;
 
-    TweenLite.set([quote1,quote2,cta], {
+    TweenLite.set([quote1, quote2, cta], {
         perspective: 100
     });
 
@@ -51,44 +51,12 @@
 
     // set defaults
 
-    // TweenLite.defaultOverwrite = 'auto';
     TweenLite.defaultEase = Cubic.easeOut;
-
-    // some eases
-
-    // ease: Elastic.easeOut.config(1, 0.3);
-    // ease: Back.easeOut.config(1.7);
-    // ease: SteppedEase.config(12);
-    // ease: SlowMo.ease.config(0.5, 1, false);
-    // ease: RoughEase.ease.config({ template: Cubic.easeOut, strength: 1, points: 20, taper: "out", randomize: true, clamp: false});
 
     var tl1 = new TimelineMax({
         repeat: 0,
         repeatDelay: frameDelay
     });
-
-    // set timeScale
-    tl1.timeScale(1);
-
-    // some timeline options
-
-    //.to(element, animTime, {});
-    //.from(element, animTime, {});
-    //.staggerTo(element, animTime, {}, staggerOffset);
-    //.staggerFrom(element, animTime, {}, staggerOffset);
-    //.set(element, {});
-
-    //cycle:{x:[50,0,-20], rotation:[30,60,90]}
-    //cycle:{x:function() { return Math.random() * 200; }}
-
-    // animate functions
-
-    // function nextFrame(nextFrameFunction, nextFrameDelay) {
-    //     setTimeout(nextFrameFunction, nextFrameDelay);
-    // }
-
-    // onComplete: nextFrame,
-    // onCompleteParams: [frame2, 2000]
 
     function startAnim() {
 
@@ -167,14 +135,7 @@
 
     function initBanner() {
         //console.log('initBanner');
-
         bannerContainer.style.display = 'block';
-
-        // banner size
-        //        vw = bannerContainer.offsetWidth;
-        //        vh = bannerContainer.offsetHeight;
-        //        console.log("size : " + vw + " x " + vh);
-
         addEventListeners();
         startAnim();
     }
@@ -182,87 +143,3 @@
     initBanner();
 
 })();
-
-// ---------------------------------------------------------------------------------
-// POLITE LOAD IMAGES
-// ---------------------------------------------------------------------------------
-
-/*
-function politeLoadImages() {
-    console.log("politeLoadImages");
-    // Call the function 
-    loadAllImages(images, function () {
-        // Do whatever you need to do when all images are loaded and assigned to their img tags....
-        console.log("allImagesLoaded");
-        // initBanner();
-    });
-}
-
-// Object with img ids and urls
-var images = {
-    'placeholder': 'images/placeholder.png',
-    'cta': 'images/cta.png',
-};
-
-function loadAllImages(images, onComplete) {
-    console.log("loadAllImages");
-    // Vars to keep track of load count
-    var loadCount = 0;
-    var imagesToLoad = 0;
-    // Determine how many images are to be loaded
-    for (var id in images) {
-        imagesToLoad++;
-    }
-
-    console.log("imagesToLoad : " + imagesToLoad);
-
-    // Iterate through the images to be loaded and load the url to an image object
-    for (var id in images) {
-        var preloadImgTag = new Image();
-        // The on load handler for each image object
-        preloadImgTag.onload = function () {
-                // Increment the load count
-                loadCount++;
-                if (loadCount === imagesToLoad) {
-                    for (var id in images) {
-                        // assign image to their associated img id src
-                        document.getElementById(id).setAttribute("src", images[id]);
-                    }
-                    // Call the on complete call back if theres one to call
-                    if (onComplete) onComplete();
-                }
-            }
-            // Initiate the load by setting the src on each image object
-        preloadImgTag.src = images[id];
-    }
-}
-
-politeLoadImages();
-*/
-
-// ---------------------------------------------------------------------------------
-// LOAD EXTERNAL JS LIBRARY
-// ---------------------------------------------------------------------------------
-
-/*
-function loadScript(url, callback) {
-    var script = document.createElement("script")
-    script.type = "text/javascript";
-    if (script.readyState) { // ie
-        script.onreadystatechange = function () {
-            if (script.readyState == "loaded" ||
-                script.readyState == "complete") {
-                script.onreadystatechange = null;
-                callback();
-            }
-        };
-    } else { // others
-        script.onload = function () {
-            callback();
-        };
-    }
-    script.src = url;
-    document.getElementsByTagName("head")[0].appendChild(script);
-}
-loadScript("libs/TweenMax.min.js", frame1);
-*/
