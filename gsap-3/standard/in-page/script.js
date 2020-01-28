37a59d35b9bc25c794c878f179fb5b89d796fc9d
Aaron Stannard <aaron@postproduktiv.com.au>
2020-01-29 10:30:13 +1100

    gsap 2 --> gsap 3

---
+++ gsap-3/standard/in-page/script.js
@@ -0,0 +1,158 @@
+(function () {
+
+    // console.clear();
+
+    // ===== get elements =====
+
+    var placeholder = document.getElementById('placeholder');
+    var cta = document.getElementById('cta');
+
+    // ===== check date =====
+
+    //    var d = new Date(); // get new date
+    //
+    //    var d1 = new Date("October 13, 2017"); // date
+    //    var d2 = new Date("October 20, 2017"); // day of week
+    //    var d3 = new Date("October 27, 2017"); // out now
+    //
+    //    console.log(d);
+    //    console.log(d1);
+    //    console.log(d2);
+    //    console.log(d3);
+    //
+    //    d = new Date("October 30, 2017"); // for testing
+    //
+    //    if (d >= d3) {
+    //        console.log('out now');
+    //        console.log(d);
+    //        console.log(d3);
+    //    } else if (d >= d2) {
+    //        console.log('day of week');
+    //        console.log(d);
+    //        console.log(d2);
+    //    } else {
+    //        console.log('date');
+    //        console.log(d);
+    //        console.log(d1);
+    //    }
+
+    // ===== set vars and defaults =====
+
+    var animTime = 1;
+    var fadeTime = animTime / 4;
+    var animDist = 100;
+    var animScale = 2;
+    var staggerOffset = 0.25;
+
+    // TweenLite.defaultOverwrite = 'auto';
+
+    var tl1 = gsap.timeline({
+        repeat: 0,
+        repeatDelay: 2
+    });
+
+    gsap.defaults({
+        ease: "power4.out"
+    });
+
+    //    gsap.config({
+    //        force3D: false
+    //    });
+
+    // ===== eases =====
+
+    // https://greensock.com/ease-visualizer
+
+    var le = "none";
+
+    var so = "sine.out";
+    var si = "sine.in";
+    var sio = "sine.inOut";
+
+    var bo = "back.out(1.7)";
+    var bi = "back.in(1.7)";
+    var bio = "back.inOut(1.7)";
+
+    var eo = "elastic.out(1, 1)"; //or just "elastic" because ".out" is the default flavor
+    var ei = "elastic.in(1, 1)";
+    var eio = "elastic.inOut(1, 1)";
+
+    // "bounce.out";
+    // "steps(5)";
+    // "slow(0.5, 0.8)";
+    // "rough(40)";
+    // "expoScale(0.5, 3)";
+
+    // ===== random =====
+
+    //chooses a random number between -100 and 100 for each target, rounding to the closest 5!
+    //gsap.to(".class", {
+    //    x: "random(-100, 100, 5)"
+    //});
+
+    //randomly selects one of the values (0, 100, 200, or 500)
+    //x: "random([0, 100, 200, 500])"
+
+    // ===== yoyo =====
+
+    //gsap.timeline({
+    //    repeat: 1,
+    //    yoyo: true
+    //});
+
+    // ===== timescale/duration =====
+    
+    // tl1.timeScale(1); 
+    // tl1.totalDuration(10);
+
+    function startAnim() {
+        //console.log('startAnim');
+
+        tl1
+
+        //.eventCallback("onStart", onStartF)
+        //.eventCallback("onUpdate", onUpdateF)
+        //.eventCallback("onComplete", onCompleteF)
+        //.eventCallback("onUpdate", null)
+
+            .addLabel("frame1", 0)
+            .addLabel("frame2", 2)
+
+        .from(placeholder, {
+            duration: animTime,
+            autoAlpha: 0,
+            scale: animScale,
+            ease: eo
+        }, "frame1")
+
+        .from(cta, {
+            duration: animTime,
+            autoAlpha: 0,
+            y: animDist,
+            ease: eo
+        }, "frame2")
+
+        ;
+
+        // ===== gets total duration of timeline =====
+
+        // console.log("duration : " + tl1.totalDuration() + " secs");
+
+    }
+
+    // ===== init =====
+
+    function addEventListeners() {
+        //console.log('addEventListeners');
+    }
+
+    function initBanner() {
+        //console.log('initBanner');
+        bannerContainer.style.display = 'block';
+        addEventListeners();
+        startAnim();
+    }
+
+    initBanner();
+
+})();
---
+++ gsap-3/video/video-inpage-970x250/logic.js
@@ -0,0 +1,303 @@
+(function () {
+
+    // console.clear();
+
+    // ---------------------------------------------------------------------------------
+    // CONTROL VARS
+    // ---------------------------------------------------------------------------------
+
+    var videonOnEndFrame = false;
+    var videoIsReplaying = false;
+
+    // ---------------------------------------------------------------------------------
+    // INIT AD COMPONENTS
+    // ---------------------------------------------------------------------------------
+
+    var mainContainer = document.getElementById('main-container');
+    var exit = document.getElementById('exit');
+
+    // ---------------------------------------------------------------------------------
+    // INIT AD
+    // ---------------------------------------------------------------------------------
+
+    function init() {
+        //addListeners();
+        //addVideoTracking();
+        if (Enabler.isPageLoaded()) {
+            show();
+        } else {
+            Enabler.addEventListener(studio.events.StudioEvent.PAGE_LOADED, show);
+        }
+    }
+
+    // ---------------------------------------------------------------------------------
+    // LISTENERS
+    // ---------------------------------------------------------------------------------
+
+    function addListeners() {
+        exit.addEventListener('click', onExitHandler);
+        videoPlayer.addEventListener('click', onExitHandler);
+    }
+
+    // ---------------------------------------------------------------------------------
+    // SHOWS THE AD
+    // ---------------------------------------------------------------------------------
+    function show() {
+        // mainContainer.style.display = "block";
+        addListeners();
+        addVideoTracking();
+    }
+
+    // ---------------------------------------------------------------------------------
+    // MAIN
+    // ---------------------------------------------------------------------------------
+
+    function onExitHandler() {
+        if (!videonOnEndFrame) {
+            pauseVideo();
+            unmuteVideo();
+            hideClickForSound();
+        }
+        Enabler.exit('Clickthrough');
+    }
+
+    // ---------------------------------------------------------------------------------
+    // ADD VIDEO SRC AND METRICS
+    // ---------------------------------------------------------------------------------
+
+    function addVideoTracking() {
+        var srcNode;
+
+        srcNode = document.createElement('source');
+
+        // srcNode.setAttribute('type', 'video/ogg');
+        // srcNode.setAttribute('src', Enabler.getUrl('video.ogg'));
+        // videoPlayer.appendChild(srcNode);
+
+        // srcNode.setAttribute('type', 'video/webm');
+        // srcNode.setAttribute('src', Enabler.getUrl('video.webm'));
+        // videoPlayer.appendChild(srcNode);
+
+        srcNode.setAttribute('type', 'video/mp4');
+        srcNode.setAttribute('src', Enabler.getUrl('video.mp4'));
+        videoPlayer.appendChild(srcNode);
+
+        Enabler.loadModule(studio.module.ModuleId.VIDEO, function () {
+            studio.video.Reporter.attach('Video', videoPlayer);
+        }.bind(this));
+
+    }
+
+    // ---------------------------------------------------------------------------------
+    // VIDEO PLAYER
+    // ---------------------------------------------------------------------------------
+
+    var videoContainer = document.getElementById('video-container');
+    var videoPlayer = document.getElementById("video-player");
+    var clickForSound = document.getElementById("click-for-sound");
+    var videoControls = document.getElementById('video-controls');
+    var playBtn = document.getElementById('play-btn');
+    var pauseBtn = document.getElementById('pause-btn');
+    var muteBtn = document.getElementById('mute-btn');
+    var unmuteBtn = document.getElementById('unmute-btn');
+    var replayBtn = document.getElementById('replay-btn');
+
+    videoPlayer.addEventListener("canplay", videoReadyToPlay, false);
+    videoPlayer.addEventListener("ended", videoEndHandler, false);
+    clickForSound.addEventListener("click", restartWithSound, false);
+
+    videoPlayer.controls = false;
+
+    gsap.defaults({
+        ease: "sine.out",
+        duration: 0.5
+    });
+
+    function showControls() {
+        gsap.to(videoControls, {
+            autoAlpha: 1
+        });
+    }
+
+    function videoReadyToPlay() {
+        showControls();
+        if (!videoIsReplaying) {
+            showClickForSound();
+        }
+    }
+
+    function restartWithSound() {
+        videoIsReplaying = true;
+        videoPlayer.currentTime = 0;
+        playVideo();
+        hideClickForSound();
+    }
+
+    function videoEndHandler() {
+        videonOnEndFrame = true;
+        pauseVideo();
+        hideClickForSound();
+        hidePlayBtn();
+        hidePauseBtn();
+        hideMuteBtn();
+        hideUnmuteBtn();
+        showReplayBtn();
+    }
+
+    // ---------------------------------------------------------------------------------
+    // VIDEO CONTROLS
+    // ---------------------------------------------------------------------------------
+
+    playBtn.addEventListener('click', playPauseHandler, false);
+    pauseBtn.addEventListener('click', playPauseHandler, false);
+    muteBtn.addEventListener('click', muteUnmuteHandler, false);
+    unmuteBtn.addEventListener('click', muteUnmuteHandler, false);
+    replayBtn.addEventListener('click', replayHandler, false);
+
+    hideReplayBtn();
+
+    function showClickForSound() {
+        gsap.to(clickForSound, {
+            autoAlpha: 1
+        });
+    }
+
+    function hideClickForSound() {
+        clickForSound.style.visibility = 'hidden';
+    }
+
+    function playVideo() {
+        videoPlayer.play();
+        hideClickForSound();
+        unmuteVideo();
+        showPauseBtn();
+        hidePlayBtn();
+    }
+
+    function pauseVideo() {
+        videoPlayer.pause();
+        showPlayBtn();
+        hidePauseBtn();
+    }
+
+    function replayVideo() {
+        videoIsReplaying = true;
+        videoPlayer.currentTime = 0;
+        unmuteVideo();
+        playVideo();
+        videonOnEndFrame = false;
+    }
+
+    function muteVideo() {
+        videoPlayer.muted = true;
+        showUnmuteBtn();
+        hideMuteBtn();
+    }
+
+    function unmuteVideo() {
+        videoPlayer.muted = false;
+        showMuteBtn();
+        hideUnmuteBtn();
+    }
+
+    function showPlayBtn() {
+        playBtn.style.visibility = 'visible';
+    }
+
+    function showPauseBtn() {
+        pauseBtn.style.visibility = 'visible';
+    }
+
+    function showReplayBtn() {
+        replayBtn.style.visibility = 'visible';
+    }
+
+    function showMuteBtn() {
+        muteBtn.style.visibility = 'visible';
+    }
+
+    function showUnmuteBtn() {
+        unmuteBtn.style.visibility = 'visible';
+    }
+
+    function hidePlayBtn() {
+        playBtn.style.visibility = 'hidden';
+    }
+
+    function hidePauseBtn() {
+        pauseBtn.style.visibility = 'hidden';
+    }
+
+    function hideReplayBtn() {
+        replayBtn.style.visibility = 'hidden';
+    }
+
+    function hideMuteBtn() {
+        muteBtn.style.visibility = 'hidden';
+    }
+
+    function hideUnmuteBtn() {
+        unmuteBtn.style.visibility = 'hidden';
+    }
+
+
+    function playPauseHandler() {
+        if (videoPlayer.paused) {
+            if (videoPlayer.muted) {
+                unmuteVideo();
+                hideClickForSound();
+            }
+            playVideo();
+        } else {
+            pauseVideo();
+        }
+    }
+
+    function muteUnmuteHandler() {
+        if (videoPlayer.muted) {
+            hideClickForSound();
+            unmuteVideo();
+        } else {
+            muteVideo();
+        }
+    }
+
+    function replayHandler() {
+        hideReplayBtn();
+        showPauseBtn();
+        showMuteBtn();
+        replayVideo();
+    }
+
+    //document.getElementById("mp4_src").src = "movie.mp4";
+    //document.getElementById("ogg_src").src = "movie.ogg";
+    //document.getElementById("myVideo").load();
+
+    //audio|video.addEventListener("loadstart", myScript);
+    //audio|video.addEventListener("canplay", myScript);
+    //audio|video.addEventListener("canplaythrough", myScript);
+    //audio|video.addEventListener("play", myScript);
+    //audio|video.addEventListener("pause", myScript);
+    //audio|video.addEventListener("ended", myScript);
+
+    //audio|video.duration
+    //audio|video.paused
+    //audio|video.ended
+    //audio|video.muted=true|false
+
+    //audio|video.play()
+    //audio|video.pause()
+
+    // ---------------------------------------------------------------------------------
+    // MAIN ON LOAD HANDLER
+    // ---------------------------------------------------------------------------------
+
+    window.onload = function () {
+        if (Enabler.isInitialized()) {
+            init();
+        } else {
+            Enabler.addEventListener(studio.events.StudioEvent.INIT, init);
+        }
+    }
+
+})();
