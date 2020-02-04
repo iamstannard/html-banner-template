3932ac5f97d27a2c628be64e40c02c720c110861
Aaron Stannard <aaron@postproduktiv.com.au>
2020-02-04 16:44:23 +1100

    Beautify

--- gsap-3/standard/in-page/script.js
+++ gsap-3/standard/in-page/script.js
@@ -74,9 +74,9 @@
     //      amount: 1 
     //    }
     //  });
-    
+
     // ===== shuffle =====
-    
+
     // var array = [1, 2, 3, 4, 5];
     // gsap.utils.shuffle(array); // returns the same array, but shuffled like [2, 5, 3, 1, 4]
 
